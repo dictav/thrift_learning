ad36c064d172083d17f7c034a8b1a38e014309e7
Shintaro Abe <dictav@gmail.com>
2016-01-09 17:22:36 +0900

    Add nodejs/browser implement

---
+++ nodejs/index.js
@@ -0,0 +1,80 @@
+var thrift = require("thrift");
+var Calculator = require("./gen-nodejs/Calculator");
+var ttypes = require("./gen-nodejs/tutorial_types");
+var SharedStruct = require("./gen-nodejs/shared_types").SharedStruct;
+
+var data = {};
+
+var handler = {
+  ping: function(result) {
+    console.log("ping()");
+    result(null);
+  },
+
+  add: function(n1, n2, result) {
+    console.log("add(", n1, ",", n2, ")");
+    result(null, n1 + n2);
+  },
+
+  calculate: function(logid, work, result) {
+    console.log("calculate(", logid, ",", work, ")");
+
+    var val = 0;
+    if (work.op == ttypes.Operation.ADD) {
+      val = work.num1 + work.num2;
+    } else if (work.op === ttypes.Operation.SUBTRACT) {
+      val = work.num1 - work.num2;
+    } else if (work.op === ttypes.Operation.MULTIPLY) {
+      val = work.num1 * work.num2;
+    } else if (work.op === ttypes.Operation.DIVIDE) {
+      if (work.num2 === 0) {
+        var x = new ttypes.InvalidOperation();
+        x.whatOp = work.op;
+        x.why = 'Cannot divide by 0';
+        result(x);
+        return;
+      }
+      val = work.num1 / work.num2;
+    } else {
+      var x = new ttypes.InvalidOperation();
+      x.whatOp = work.op;
+      x.why = 'Invalid operation';
+      result(x);
+      return;
+    }
+
+    var entry = new SharedStruct();
+    entry.key = logid;
+    entry.value = ""+val;
+    data[logid] = entry;
+
+    result(null, val);
+  },
+
+  getStruct: function(key, result) {
+    console.log("getStruct(", key, ")");
+    result(null, data[key]);
+  },
+
+  zip: function() {
+    console.log("zip()");
+    result(null);
+  }
+
+};
+
+var opt = {
+  handler: handler,
+  processor: Calculator,
+  protocol: thrift.TJSONProtocol,
+  transport: thrift.TBufferedTransport
+}
+
+var routes = {
+  services: {
+    "/calc": opt
+  }
+}
+
+thrift.createWebServer(routes).listen(9090);
+console.log("Http/Thrift Server running on port: 9090");
